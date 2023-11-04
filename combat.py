9f8a06088540f1b211926249b9c4a653ebc60f66
WolfPai <82676269+Wolf-Pai@users.noreply.github.com>
2023-11-04 00:41:52 -0400

    Create combat.py
    Sketched an outline for combat

---
+++ combat.py
@@ -0,0 +1,38 @@
+import os, sys
+
+def combat_begin():
+  # Needs to init some kind of file, storing init
+    # Look into Classes for this... or somehow define a global var that can help differentiate between files
+
+def combat_join():
+  # Join combat with standard rule (CR 3.0)
+  # Remember that joining during mid-combat is permitted, but will have to take their first move last
+  # One-sided is Clash Win
+  # Tie is do again -> recursive?
+
+def action():
+  # Melee Attack
+  # Ranged Attack
+  # Protect
+  # Reduce Status
+  # Dash
+  # Disengage
+  # Use Tool
+  # Grapple
+  # Take Cover
+  # Hide
+  # Change Weapons
+  # Convert to Reaction
+  # Delay
+
+  # Somehow give prompt to target for Clash
+
+
+def reaction():
+  # Do you want to react to <Action>?
+  # Button: yes/no
+  # On yes: do block/counter/evade
+  # Special case: Opportunity attack
+
+def combat_end():
+  # Only initiator can end
